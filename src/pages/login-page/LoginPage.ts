4607cab67b73a2c574bc8902990d27275f3b3414
solidados <geekbrains.pk@internet.ru>
2023-08-08 20:44:29 +0400

    refactor: styles for nav and login/ registration pages

--- src/pages/login-page/LoginPage.ts
+++ src/pages/login-page/LoginPage.ts
@@ -4,26 +4,26 @@ import { ProjectPages } from '../../types/Enums';
 class LoginPage extends Page {
   private LOGIN_PAGE_MARKUP =
     `<div class="container-login">
-  <div class="main-box login">
-    <h2>Login</h2>
-    <form id="login-form">
-      <div class="input-box">
-        <span class="icon"><i class='bx bxs-envelope'></i></span>
-        <input type="email" name="email" required>
-        <label for="email">Email</label>
+      <div class="main-box login">
+        <h2>Login</h2>
+        <form id="login-form">
+          <div class="input-box">
+            <span class="icon"><i class='bx bxs-envelope'></i></span>
+            <input type="email" name="email" required>
+            <label for="email">Email</label>
+          </div>
+          <div class="input-box">
+            <span class="icon icon-lock"><i class='bx bxs-lock-alt'></i></span>
+            <input type="password" name="password" class="input-password" required>
+            <label for="password">Password</label>
+          </div>
+          <button class="main-btn" type="submit">Login</button>
+          <div class="register">
+            <p>New customer?<a href="#registration-page" class="register-link">Register</a></p>
+          </div>
+        </form>
       </div>
-      <div class="input-box">
-        <span class="icon icon-lock"><i class='bx bxs-lock-alt'></i></span>
-        <input type="password" name="password" class="input-password" required>
-        <label for="password">Password</label>
-      </div>
-      <button class="main-btn" type="submit">Login</button>
-      <div class="register">
-        <p>New customer?<a href="#registration-page" class="register-link">Register</a></p>
-      </div>
-    </form>
-  </div>
-</div>`;
+    </div>`;
 
   constructor() {
     super(ProjectPages.LoginPage);
