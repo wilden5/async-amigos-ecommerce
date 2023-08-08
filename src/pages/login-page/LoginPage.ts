13116a5d3e78bd7c9e57822bf89af4117ce7fef1
solidados <geekbrains.pk@internet.ru>
2023-08-08 19:33:45 +0400

    feat: add LoginPage & RegistrationPage layout; add styles

--- src/pages/login-page/LoginPage.ts
+++ src/pages/login-page/LoginPage.ts
@@ -3,7 +3,31 @@ import { ProjectPages } from '../../types/Enums';
 
 class LoginPage extends Page {
   private LOGIN_PAGE_MARKUP = `
-     <h1 class='header'>Login Page</h1>`;
+    <div class="container-login">
+      <div class="main-box login">
+        <h2>Login</h2>
+        <form id="login-form">
+          <div class="input-box">
+            <span class="icon"><i class='bx bxs-envelope'></i></span>
+            <input type="email" name="email" required>
+            <label for="email">Email</label>
+          </div>
+
+          <div class="input-box">
+            <span class="icon icon-lock"><i class='bx bxs-lock-alt'></i></span>
+            <input type="password" name="password" class="input-password" required>
+            <label for="password">Password</label>
+          </div>
+
+          <button class="main-btn" type="submit">Login</button>
+
+          <div class="register">
+            <p>New customer?<a href="#registration-page" class="register-link">Register</a></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  `;
 
   constructor() {
     super(ProjectPages.LoginPage);
--- src/pages/registration-page/RegistrationPage.ts
+++ src/pages/registration-page/RegistrationPage.ts
@@ -3,7 +3,44 @@ import { ProjectPages } from '../../types/Enums';
 
 class RegistrationPage extends Page {
   private REGISTRATION_PAGE_MARKUP = `
-     <h1 class='header'>Registration Page</h1>`;
+    <div class="container-login">
+      <div class="main-box register">
+        <h2>Registration</h2>
+        <form id="register-form">
+
+          <div class="input-box">
+            <span class="icon"><i class='bx bxs-user'></i></span>
+            <input type="text" name="userName" required>
+            <label for="userName">Username</label>
+          </div>
+
+          <div class="input-box">
+            <span class="icon"><i class='bx bxs-envelope'></i></span>
+            <input type="email" name="email" required>
+            <label for="email">Email</label>
+          </div>
+
+          <div class="input-box">
+            <span class="icon icon-lock"><i class='bx bxs-lock-alt'></i></span>
+            <input type="password" name="password" class="input-password" required>
+            <label for="password">Password</label>
+          </div>
+
+          <div class="check">
+            <label for="rememberMe">
+              <input type="checkbox" name="acceptTerms">I accept terms and conditions</input>
+            </label>
+          </div>
+
+          <button class="main-btn" type="submit">Register me</button>
+
+          <div class="register">
+            <p>Already a Customer?<a href="#login-page" class="login-link">Login</a></p>
+          </div>
+        </form>
+      </div>
+    </div>
+  `;
 
   constructor() {
     super(ProjectPages.RegistrationPage);
