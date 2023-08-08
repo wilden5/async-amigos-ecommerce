27e700c97e52b6812ab0df8f50f11347f1f02eab
solidados <geekbrains.pk@internet.ru>
2023-08-08 20:18:39 +0400

    chore: add boxicons package; change expected strings in __test__

--- __tests__/pages/login-page/LoginPage.test.ts
+++ __tests__/pages/login-page/LoginPage.test.ts
@@ -1,25 +1,46 @@
 import LoginPage from '../../../src/pages/login-page/LoginPage';
 
-describe('LoginPage', () => {
+describe('LoginPage', (): void => {
   let loginPage: LoginPage;
   const containerId = 'test-container';
-  const expectedLoginPageMarkup = `<h1 class="header">Login Page</h1>`;
+  const expectedLoginPageMarkup =
+  `<div class="container-login">
+    <div class="main-box login">
+      <h2>Login</h2>
+      <form id="login-form">
+        <div class="input-box">
+          <span class="icon"><i class='bx bxs-envelope'></i></span>
+          <input type="email" name="email" required>
+          <label for="email">Email</label>
+        </div>
+        <div class="input-box">
+          <span class="icon icon-lock"><i class='bx bxs-lock-alt'></i></span>
+          <input type="password" name="password" class="input-password" required>
+          <label for="password">Password</label>
+        </div>
+        <button class="main-btn" type="submit">Login</button>
+        <div class="register">
+          <p>New customer?<a href="#registration-page" class="register-link">Register</a></p>
+        </div>
+      </form>
+    </div>
+  </div>`;
 
-  beforeEach(() => {
+  beforeEach((): void => {
     const container = document.createElement('div');
     container.id = containerId;
     document.body.appendChild(container);
     loginPage = new LoginPage();
   });
 
-  afterEach(() => {
+  afterEach((): void => {
     const container = document.querySelector(`#${containerId}`);
     if (container) {
       document.body.removeChild(container);
     }
   });
 
-  test('renderPage should render the login page markup', () => {
+  test('renderPage should render the login page markup', (): void => {
     let container = document.querySelector(`#${containerId}`);
     expect(container?.innerHTML).toBe('');
 
--- __tests__/pages/registration-page/RegistrationPage.test.ts
+++ __tests__/pages/registration-page/RegistrationPage.test.ts
@@ -1,25 +1,56 @@
 import RegistrationPage from '../../../src/pages/registration-page/RegistrationPage';
 
-describe('RegistrationPage', () => {
+describe('RegistrationPage', (): void => {
   let registrationPage: RegistrationPage;
   const containerId = 'test-container';
-  const expectedRegistrationPageMarkup = `<h1 class="header">Registration Page</h1>`;
+  const expectedRegistrationPageMarkup =
+  `<div class="container-login">
+    <div class="main-box register">
+      <h2>Registration</h2>
+      <form id="register-form">
+        <div class="input-box">
+          <span class="icon"><i class='bx bxs-user'></i></span>
+          <input type="text" name="userName" required>
+          <label for="userName">Username</label>
+        </div>
+        <div class="input-box">
+          <span class="icon"><i class='bx bxs-envelope'></i></span>
+          <input type="email" name="email" required>
+          <label for="email">Email</label>
+        </div>
+        <div class="input-box">
+          <span class="icon icon-lock"><i class='bx bxs-lock-alt'></i></span>
+          <input type="password" name="password" class="input-password" required>
+          <label for="password">Password</label>
+        </div>
+        <div class="check">
+          <label for="rememberMe">
+            <input type="checkbox" name="acceptTerms">I accept terms and conditions</input>
+          </label>
+        </div>
+        <button class="main-btn" type="submit">Register me</button>
+        <div class="register">
+          <p>Already a Customer?<a href="#login-page" class="login-link">Login</a></p>
+        </div>
+      </form>
+    </div>
+  </div>`;
 
-  beforeEach(() => {
+  beforeEach((): void => {
     const container = document.createElement('div');
     container.id = containerId;
     document.body.appendChild(container);
     registrationPage = new RegistrationPage();
   });
 
-  afterEach(() => {
+  afterEach((): void => {
     const container = document.querySelector(`#${containerId}`);
     if (container) {
       document.body.removeChild(container);
     }
   });
 
-  test('renderPage should render the registration page markup', () => {
+  test('renderPage should render the registration page markup', (): void => {
     let container = document.querySelector(`#${containerId}`);
     expect(container?.innerHTML).toBe('');
 
