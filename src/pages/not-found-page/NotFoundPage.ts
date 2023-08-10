ab4a5589b2b81412eb4656078945d6407594172d
solidados <geekbrains.pk@internet.ru>
2023-08-10 18:15:09 +0400

    refactor: in config change rule; in layout add import image; add types.d.ts for image types

--- src/pages/not-found-page/NotFoundPage.ts
+++ src/pages/not-found-page/NotFoundPage.ts
@@ -1,12 +1,14 @@
 import Page from '../../components/templates/Page';
 import { ProjectPages } from '../../types/Enums';
+import notFoundImage from '../../assets/not-found-image.png';
 
 class NotFoundPage extends Page {
+
   private NOT_FOUND_PAGE_MARKUP = `
-    <h2 class="error-header" data-text="404">404</h2>
-    <img src='../../assets/not-found-image.png' alt='grumpy-cat'>
+  <h2 class="error-header" data-text="404">404</h2>
+    <img src="${ notFoundImage }" alt="grumpy-cat">
     <p class="error-message">Oops! The page you're looking for doesn't exist.</p>
-    <a class="navigate-to-home" href="/#">Go back to homepage</a>`;
+    <a class="navigate-to-home" href="/#">Return to homepage</a>`;
 
   constructor() {
     super(ProjectPages.NotFound);
