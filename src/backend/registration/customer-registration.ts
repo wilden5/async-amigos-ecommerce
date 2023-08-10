c69a96f703301cee5df1a62d45c7d1aad65a934e
petrkanapelka <kanapelka-petr@mail.ru>
2023-08-10 19:44:43 +0300

    feat: create classes for registration and singn in

---
+++ src/backend/registration/customer-registration.ts
@@ -0,0 +1,27 @@
+import { ClientResponse, CustomerDraft, CustomerSignInResult } from '@commercetools/platform-sdk';
+import { CtpClient } from '../ctpClient/ctpClient';
+
+export class CustomerRegistration {
+  private ctpClient: CtpClient;
+
+  private customerData: CustomerDraft;
+
+  constructor(customerData: CustomerDraft) {
+    this.ctpClient = new CtpClient();
+    this.customerData = customerData;
+  }
+
+  async createCustomer(): Promise<ClientResponse<CustomerSignInResult>> {
+    if (this.customerData.email && this.customerData.password) {
+      const response = await this.ctpClient
+        .withPasswordFlow(this.customerData.email, this.customerData.password)
+        .customers()
+        .post({
+          body: this.customerData,
+        })
+        .execute();
+      return response;
+    }
+    throw new Error('Email or password is missing');
+  }
+}
