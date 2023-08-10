67e32969399b0dc0453d3856caf4b0ad63009a0b
petrkanapelka <kanapelka-petr@mail.ru>
2023-08-10 19:27:47 +0300

    feat: create ctpClient class

--- src/backend/ctpClient/ctpClient.ts
+++ src/backend/ctpClient/ctpClient.ts
@@ -1,4 +1,4 @@
-import { ClientBuilder, PasswordAuthMiddlewareOptions } from '@commercetools/sdk-client-v2';
+import { AuthMiddlewareOptions, ClientBuilder, PasswordAuthMiddlewareOptions } from '@commercetools/sdk-client-v2';
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 import { ByProjectKeyRequestBuilder } from '@commercetools/platform-sdk/dist/declarations/src/generated/client/by-project-key-request-builder';
 import { type HttpMiddlewareOptions } from '@commercetools/sdk-client-v2';
@@ -32,7 +32,6 @@ export class CtpClient {
           password,
         },
       },
-      // Исправленный код
       scopes,
       fetch,
     };
@@ -42,5 +41,19 @@ export class CtpClient {
     const apiRootPassword = createApiBuilderFromCtpClient(this.ctpClient).withProjectKey({ projectKey });
 
     return apiRootPassword;
+  }
+
+  withClientCredentialsFlow(): ClientBuilder {
+    const authMiddlewareOptions: AuthMiddlewareOptions = {
+      host,
+      projectKey,
+      credentials: {
+        clientId,
+        clientSecret,
+      },
+      scopes,
+      fetch,
+    };
+    return this.ctpClient.withClientCredentialsFlow(authMiddlewareOptions);
   }
 }
