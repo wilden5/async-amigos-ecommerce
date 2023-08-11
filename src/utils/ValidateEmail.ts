7e78511c6a3fd189181767c393e2face6f3b0fa8
solidados <geekbrains.pk@internet.ru>
2023-08-11 23:22:47 +0400

    refactor: add class and method for email and password validation

--- src/utils/ValidateEmail.ts
+++ src/utils/ValidateEmail.ts
@@ -1,12 +1,14 @@
 import { EmailHints } from '../types/Enums';
 
-function validateEmail(email: string): string | boolean {
-  // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
-    return EmailHints.EmailFormatHint;
-  }
+class EmailValidator {
+  static validate(email: string): string | boolean {
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+      return EmailHints.EmailFormatHint;
+    }
 
-  return true;
+    return true;
+  }
 }
 
-export default validateEmail;
+export default EmailValidator;
--- src/utils/ValidatePassword.ts
+++ src/utils/ValidatePassword.ts
@@ -1,31 +1,33 @@
 /* eslint-disable no-nested-ternary */
 import { PasswordHints } from '../types/Enums';
 
-function validatePassword(password: string): string | boolean {
-  if (password.length < 8) {
-    return PasswordHints.LengthHint;
-  }
+class PasswordValidator {
+  static validate(password: string): string | boolean {
+    if (password.length < 8) {
+      return PasswordHints.LengthHint;
+    }
 
-  if (!/[A-Z]/.test(password)) {
-    return PasswordHints.UpperLettersHint;
-  }
+    if (!/[A-Z]/.test(password)) {
+      return PasswordHints.UpperLettersHint;
+    }
 
-  if (!/[a-z]/.test(password)) {
-    return PasswordHints.LowerLettersHint;
-  }
+    if (!/[a-z]/.test(password)) {
+      return PasswordHints.LowerLettersHint;
+    }
 
-  if (!/[0-9]/.test(password)) {
-    return PasswordHints.DigitsHint;
-  }
+    if (!/[0-9]/.test(password)) {
+      return PasswordHints.DigitsHint;
+    }
 
-  if (/\s/.test(password)) {
-    return PasswordHints.SpacesHint;
-  }
+    if (/\s/.test(password)) {
+      return PasswordHints.SpacesHint;
+    }
 
-  if (!/[!@#$%^&*]/.test(password)) {
-    return PasswordHints.SpecialCharsHint;
+    if (!/[!@#$%^&*]/.test(password)) {
+      return PasswordHints.SpecialCharsHint;
+    }
+    return true;
   }
-  return true;
 }
 
-export default validatePassword;
+export default PasswordValidator;
