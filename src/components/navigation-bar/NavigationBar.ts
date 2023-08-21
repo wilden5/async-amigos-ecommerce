5bc412b55ea862472e8ddd990acc2b58cd40f96a
Denis Ivanchenko <sicknature24@yandex.com>
2023-08-21 23:23:23 +0300

    feat: add logout functionality for authenticated user

--- src/components/navigation-bar/NavigationBar.ts
+++ src/components/navigation-bar/NavigationBar.ts
@@ -1,25 +1,60 @@
 import Component from '../templates/Component';
 import Constants from '../../utils/Constants';
+import LocalStorage from '../../utils/LocalStorage';
+import { ProjectPages } from '../../types/Enums';
 
 class NavigationBar extends Component {
-  public NAVIGATION_BAR_MARKUP = `
+  private LOCAL_STORAGE: LocalStorage;
+
+  public NAVIGATION_BAR_MARKUP_GUEST = `
+    <ul class="${Constants.NAVIGATION_BAR_SELECTOR}__list">
+      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#'>Home</a></li>
+      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#catalog'>Catalog</a></li>
+      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#about-us'>About Us</a></li>
+      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#cart'>Cart</a></li>
+      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#login'>Login</a></li>
+      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#registration'>Registration</a></li>
+    </ul>
+  `;
+
+  public NAVIGATION_BAR_MARKUP_AUTH_USER = `
     <ul class="${Constants.NAVIGATION_BAR_SELECTOR}__list">
       <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#'>Home</a></li>
       <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#catalog'>Catalog</a></li>
       <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#about-us'>About Us</a></li>
       <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#cart'>Cart</a></li>
       <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#my-profile'>My Profile</a></li>
-      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#login'>Login</a></li>
-      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item"><a href='#registration'>Registration</a></li>
+      <li class="${Constants.NAVIGATION_BAR_SELECTOR}__item user-logout"><a href='#'>Logout</a></li>
     </ul>
   `;
 
   constructor() {
     super('nav', `${Constants.NAVIGATION_BAR_SELECTOR}`);
+    this.LOCAL_STORAGE = new LocalStorage();
+  }
+
+  private handleLogoutLinkClick(): void {
+    if (this.LOCAL_STORAGE.isLocalStorageItemExists(Constants.SUCCESSFUL_REGISTRATION_LOCAL_STORAGE_KEY)) {
+      (document.querySelector('.user-logout') as HTMLAnchorElement).addEventListener('click', () => {
+        this.LOCAL_STORAGE.removeLocalStorageItem(Constants.SUCCESSFUL_REGISTRATION_LOCAL_STORAGE_KEY);
+        window.location.href = ProjectPages.Home;
+      });
+    }
+  }
+
+  private handleNavigationBarType(): void {
+    if (this.LOCAL_STORAGE.isLocalStorageItemExists(Constants.SUCCESSFUL_REGISTRATION_LOCAL_STORAGE_KEY)) {
+      this.CONTAINER.innerHTML = this.NAVIGATION_BAR_MARKUP_AUTH_USER;
+      setTimeout(() => {
+        this.handleLogoutLinkClick();
+      }, 500);
+    } else {
+      this.CONTAINER.innerHTML = this.NAVIGATION_BAR_MARKUP_GUEST;
+    }
   }
 
   public renderComponent(): HTMLElement {
-    this.CONTAINER.innerHTML = this.NAVIGATION_BAR_MARKUP;
+    this.handleNavigationBarType();
     return this.CONTAINER;
   }
 }
