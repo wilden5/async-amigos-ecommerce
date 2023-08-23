0d8af84371e0b91537524dbf34493c91e6e5a1c9
Denis Ivanchenko <sicknature24@yandex.com>
2023-08-23 14:24:51 +0300

    feat: add backend method to query products

--- src/pages/catalog-page/CatalogPage.ts
+++ src/pages/catalog-page/CatalogPage.ts
@@ -1,16 +1,32 @@
 import Page from '../../components/templates/Page';
 import { ProjectPages } from '../../types/Enums';
+import QueryProducts from '../../backend/products/QueryProducts';
 
 class CatalogPage extends Page {
   private CATALOG_PAGE_MARKUP = `
-     <h1 class='page-title'>Catalog Page</h1>`;
+     <h1 class='page-title'>Catalog Page</h1>
+     <div class='list-container'></div>`;
 
   constructor() {
     super(ProjectPages.Catalog);
   }
 
+  private getProductList(): void {
+    new QueryProducts()
+      .queryProductList()
+      .then((abc) => {
+        const container = this.CONTAINER.querySelector('.list-container') as HTMLDivElement;
+
+        container.innerHTML = JSON.stringify(abc);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   public renderPage(): HTMLElement {
     this.CONTAINER.innerHTML = this.CATALOG_PAGE_MARKUP;
+    this.getProductList();
     return this.CONTAINER;
   }
 }
