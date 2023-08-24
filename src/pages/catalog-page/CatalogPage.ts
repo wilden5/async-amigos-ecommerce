6c350acbd957042321cb27a5727bf40bf4c38cff
Denis Ivanchenko <sicknature24@yandex.com>
2023-08-24 12:30:59 +0300

    feat: add draft version of product item

--- src/pages/catalog-page/CatalogPage.ts
+++ src/pages/catalog-page/CatalogPage.ts
@@ -4,8 +4,8 @@ import QueryProducts from '../../backend/products/QueryProducts';
 
 class CatalogPage extends Page {
   private CATALOG_PAGE_MARKUP = `
-     <h1 class='page-title'>Catalog Page</h1>
-     <div class='list-container'></div>`;
+     <h1 class='page-title'>Search results:</h1>
+     <div class='product-container'></div>`;
 
   constructor() {
     super(ProjectPages.Catalog);
@@ -15,9 +15,27 @@ class CatalogPage extends Page {
     new QueryProducts()
       .queryProductList()
       .then((abc) => {
-        const container = this.CONTAINER.querySelector('.list-container') as HTMLDivElement;
+        const container = this.CONTAINER.querySelector('.product-container') as HTMLDivElement;
 
-        container.innerHTML = JSON.stringify(abc);
+        abc.results.forEach((product) => {
+          const productKey = product.key as string;
+          const productName = product.masterData.current.name['en-US'];
+          const productDescription = product.masterData.current.description?.['en-US'];
+          const { url: imageURL, label: imageLabel } = product.masterData.current.masterVariant.images?.[0] ?? {
+            url: '',
+            label: '',
+          };
+
+          const productElement = document.createElement('div');
+          productElement.className = `${product.id} product-item`;
+          productElement.innerHTML = `
+            <img class="${productKey} product-image" src="${imageURL}" alt="${imageLabel || ''}">
+            <h2 class="${productKey} product-title">${productName}</h2>
+            <p class="${productKey} product-description">${
+              productDescription || ''
+            }</p> <a class='order-me' href=#product/${productKey} target='_blank'>ADD TO CART</a>`;
+          container.appendChild(productElement);
+        });
       })
       .catch((error) => {
         console.error(error);
