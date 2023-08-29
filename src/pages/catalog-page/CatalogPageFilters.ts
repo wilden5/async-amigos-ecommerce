1f5607536b17295d9b22b8bdeed24fc3ce49972b
Denis Ivanchenko <sicknature24@yandex.com>
2023-08-29 14:10:53 +0300

    feat: implement filterProductCatalog method to perform search based on filter query

--- src/pages/catalog-page/CatalogPageFilters.ts
+++ src/pages/catalog-page/CatalogPageFilters.ts
@@ -1,7 +1,10 @@
-import { ProductType, ProductTypePagedQueryResponse } from '@commercetools/platform-sdk';
+import { ProductProjection, ProductType, ProductTypePagedQueryResponse } from '@commercetools/platform-sdk';
+import { ProductProjectionPagedSearchResponse } from '@commercetools/platform-sdk/dist/declarations/src/generated/models/product';
 import QueryProducts from '../../backend/products/QueryProducts';
 import PromiseHelpers from '../../utils/PromiseHelpers';
 import Constants from '../../utils/Constants';
+import ProductProjectionSearch from '../../backend/products/ProductProjectionSearch';
+import ProductCardBuilder from './ProductCardBuilder';
 
 class CatalogPageFilters {
   static populateProductTypesSelect(container: HTMLElement): void {
@@ -11,7 +14,7 @@ class CatalogPageFilters {
       .then((queriedProductTypes: ProductTypePagedQueryResponse) => {
         queriedProductTypes.results.forEach((type: ProductType) => {
           const productTypeOption = document.createElement('option');
-          productTypeOption.value = type.key as string;
+          productTypeOption.value = type.id;
           productTypeOption.textContent = type.name;
           typeSelect.appendChild(productTypeOption);
         });
@@ -21,8 +24,27 @@ class CatalogPageFilters {
       });
   }
 
+  static performFilterByProductType(container: HTMLElement): void {
+    (container.querySelector('.type-select') as HTMLSelectElement).addEventListener('change', () => {
+      const selectedValue = (container.querySelector('.type-select') as HTMLSelectElement).value;
+      new ProductProjectionSearch()
+        .filterProductCatalog(`productType.id:"${selectedValue}"`)
+        .then((queriedProductList: ProductProjectionPagedSearchResponse): void => {
+          const productContainer = container.querySelector('.product-container') as HTMLElement;
+          productContainer.innerHTML = '';
+          queriedProductList.results.forEach((product: ProductProjection): void => {
+            ProductCardBuilder.buildProductCard(product, productContainer);
+          });
+        })
+        .catch((error: Error): void => {
+          PromiseHelpers.catchBlockHelper(error, Constants.FETCH_CATALOG_ERROR);
+        });
+    });
+  }
+
   static initAllFilters(container: HTMLElement): void {
     CatalogPageFilters.populateProductTypesSelect(container);
+    CatalogPageFilters.performFilterByProductType(container);
   }
 }
 
