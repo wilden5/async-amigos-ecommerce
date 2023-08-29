13053c57cd66228b2a2ddee714c16a586e92b7ef
solidados <geekbrains.pk@internet.ru>
2023-08-29 14:45:29 +0400

    chore: create Slider as component

---
+++ src/components/slider/Slider.ts
@@ -0,0 +1,91 @@
+// import { Product } from '@commercetools/platform-sdk';
+// import Constants from '../../utils/Constants';
+import DOMHelpers from '../../utils/DOMHelpers';
+import Page from '../templates/Page';
+import { ProjectPages } from '../../types/Enums';
+
+class Slider extends Page {
+  private images: string[];
+
+  private currentsSlideIndex: number;
+
+  constructor(images: string[]) {
+    super(ProjectPages.ProductDetails);
+    this.images = images;
+    this.currentsSlideIndex = 0;
+  }
+
+  // private getAllProductImages(product: Product): void {
+  //   const { url: imageURL, label: imageLabel } = product.masterData.current.masterVariant.images?.[0] ?? {
+  //     url: Constants.IMAGE_NOT_FOUND_MOCK_IMAGE,
+  //     label: Constants.IMAGE_NOT_FOUND_LABEL,
+  //   };
+  // }
+
+  private createSlide(imgURL: string): HTMLElement {
+    const slide: HTMLElement = DOMHelpers.createElement('swiper-slide', { className: 'mySwiper__item' });
+    slide.innerHTML = `
+     <img src="${imgURL}" alt="product">
+    `;
+    return slide;
+  }
+
+  private createControlButton(text: string, onClick: () => void): HTMLElement {
+    const sliderBtn: HTMLElement = DOMHelpers.createElement('button', {
+      className: 'mySwiper-control-btn',
+      textContent: text,
+    });
+    sliderBtn.addEventListener('click', onClick);
+    return sliderBtn;
+  }
+
+  private showSlide(index: number): void {
+    const slides: NodeListOf<Element> = this.CONTAINER.querySelectorAll('.swiper-slide');
+    slides.forEach((slide: Element, idx: number): void => {
+      slide.classList.toggle('active', idx === index);
+    });
+    this.currentsSlideIndex = index;
+  }
+
+  private setAttributes(element: HTMLElement, attributes: { [key: string]: string }): void {
+    Object.entries(attributes).forEach(([key, value]): void => {
+      element.setAttribute(key, value);
+    });
+  }
+
+  private createSliderContainer(): HTMLElement {
+    const slidesContainer: HTMLElement = DOMHelpers.createElement('swiper-container', { className: 'mySwiper__list' });
+    this.setAttributes(slidesContainer, {
+      'pagination': 'true',
+      'pagination-type': 'progressbar',
+      'navigation': 'true',
+    });
+    return slidesContainer;
+  }
+
+  public render(): HTMLElement {
+    const slider: HTMLElement = this.createSliderContainer();
+    this.images.forEach((imageURL: string): void => {
+      const slide: HTMLElement = this.createSlide(imageURL);
+      slider.appendChild(slide);
+    });
+
+    const prevButton: HTMLElement = this.createControlButton('<', (): void => {
+      const newIndex: number = (this.currentsSlideIndex - 1 + this.images.length) % this.images.length;
+      this.showSlide(newIndex);
+    });
+
+    const nextButton: HTMLElement = this.createControlButton('>', (): void => {
+      const newIndex: number = (this.currentsSlideIndex + 1) % this.images.length;
+      this.showSlide(newIndex);
+    });
+    slider.appendChild(prevButton);
+    slider.appendChild(nextButton);
+
+    this.showSlide(0);
+
+    return slider;
+  }
+}
+
+export default Slider;
