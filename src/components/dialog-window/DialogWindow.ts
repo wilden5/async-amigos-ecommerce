0a08fcc14c1363616c2c8f2f190dd4cbb7118ce7
solidados <geekbrains.pk@internet.ru>
2023-08-31 23:17:10 +0400

    feat: add mock:jest in App.test.js for Slider

--- src/components/dialog-window/DialogWindow.ts
+++ src/components/dialog-window/DialogWindow.ts
@@ -1,72 +1,73 @@
-// import Component from '../templates/Component';
-// import DOMHelpers from '../../utils/DOMHelpers';
-// import Slider from '../slider/Slider';
-//
-// class DialogWindow extends Component {
-//   private readonly dialogElement: HTMLDivElement;
-//
-//   private readonly overlayElement: HTMLDivElement;
-//
-//   private slider: Slider | null;
-//
-//   constructor() {
-//     super('div', 'dialog-window');
-//     this.dialogElement = this.createDialogElement();
-//     this.overlayElement = this.createOverlayElement();
-//     this.slider = null;
-//   }
-//
-//   private createDialogElement(): HTMLDivElement {
-//     const dialog = DOMHelpers.createElement('div', { className: 'dialog' }) as HTMLDivElement;
-//     dialog.innerHTML = `
-//       <div class="dialog-content">
-//         <div class="slider-container"></div>
-//         <button class="dialog-close">Close</button>
-//         <button class="dialog-close-window">Back to Catalog</button>
-//       </div>
-//     `;
-//     return dialog;
-//   }
-//
-//   private createOverlayElement(): HTMLDivElement {
-//     return DOMHelpers.createElement('div', 'overlay') as HTMLDivElement;
-//   }
-//
-//   private closeDialog(): void {
-//     if (this.slider) {
-//       this.slider.destroy();
-//       this.slider = null;
-//     }
-//
-//     if (document.body.contains(this.dialogElement)) {
-//       document.body.removeChild(this.dialogElement);
-//     }
-//
-//     if (document.body.contains(this.overlayElement)) {
-//       document.body.removeChild(this.overlayElement);
-//     }
-//   }
-//
-//   private closeDialogIfOverlayClicked(event: MouseEvent): void {
-//     if (event.target === this.overlayElement) {
-//       this.closeDialog();
-//     }
-//   }
-//
-//   public openProductImages(imageURLs: string[], productName: string): void {
-//     const sliderContainer = this.dialogElement.querySelector('.slider-container') as HTMLDivElement;
-//     if (sliderContainer) {
-//       this.slider = new Slider();
-//       sliderContainer.appendChild(this.slider.renderComponent(imageURLs));
-//     }
-//
-//     document.body.appendChild(this.overlayElement);
-//     document.body.appendChild(this.dialogElement);
-//   }
-//
-//   public renderComponent(): HTMLElement {
-//     return this.dialogElement;
-//   }
-// }
-//
-// export default DialogWindow;
+import Component from '../templates/Component';
+import DOMHelpers from '../../utils/DOMHelpers';
+import Slider from '../slider/Slider';
+
+class DialogWindow extends Component {
+  private readonly dialogElement: HTMLDivElement;
+
+  private readonly overlayElement: HTMLDivElement;
+
+  private slider: Slider | null;
+
+  constructor() {
+    super('div', 'dialog-window');
+    this.dialogElement = this.createDialogElement();
+    this.overlayElement = this.createOverlayElement();
+    this.slider = null;
+  }
+
+  private createDialogElement(): HTMLDivElement {
+    const dialog = DOMHelpers.createElement('div', { className: 'dialog' }) as HTMLDivElement;
+    dialog.innerHTML = `
+      <div class="dialog-content">
+        <div class="slider-container"></div>
+        <button class="dialog-close">Close</button>
+        <button class="dialog-close-window">Back to Catalog</button>
+      </div>
+    `;
+    return dialog;
+  }
+
+  private createOverlayElement(): HTMLDivElement {
+    return DOMHelpers.createElement('div', 'overlay') as HTMLDivElement;
+  }
+
+  private closeDialog(): void {
+    if (this.slider) {
+      this.slider.destroy();
+      this.slider = null;
+    }
+
+    if (document.body.contains(this.dialogElement)) {
+      document.body.removeChild(this.dialogElement);
+    }
+
+    if (document.body.contains(this.overlayElement)) {
+      document.body.removeChild(this.overlayElement);
+    }
+  }
+
+  private closeDialogIfOverlayClicked(event: MouseEvent): void {
+    if (event.target === this.overlayElement) {
+      this.closeDialog();
+    }
+  }
+
+  // TODO: these are the props for openProductImages (imageURLs: string[], productName: string)
+  public openProductImages(): void {
+    const sliderContainer = this.dialogElement.querySelector('.slider-container') as HTMLDivElement;
+    if (sliderContainer) {
+      this.slider = new Slider();
+      sliderContainer.appendChild(this.slider.renderComponent());
+    }
+
+    document.body.appendChild(this.overlayElement);
+    document.body.appendChild(this.dialogElement);
+  }
+
+  public renderComponent(): HTMLElement {
+    return this.dialogElement;
+  }
+}
+
+export default DialogWindow;
