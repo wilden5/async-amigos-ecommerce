47ee45c7f0554744c448c6e8d35ea5884813244b
Denis Ivanchenko <sicknature24@yandex.com>
2023-09-11 13:06:42 +0300

    feat: handle guest sign-in; remove token property from local storage after log out

--- src/backend/login/CustomerLogin.ts
+++ src/backend/login/CustomerLogin.ts
@@ -1,15 +1,21 @@
 import { ClientResponse, CustomerSignInResult, CustomerSignin } from '@commercetools/platform-sdk';
 import { CtpClient } from '../ctpClient/ctpClient';
 import LoginValidator from './LoginValidator';
+import Authorization from '../auth/Authorization';
+import LocalStorage from '../../utils/LocalStorage';
+import Constants from '../../utils/Constants';
 
 export class CustomerLogin {
   private readonly loginData: CustomerSignin;
+
+  private LOCAL_STORAGE: LocalStorage;
 
   public ctpClient: CtpClient;
 
   constructor(loginData: CustomerSignin) {
     this.ctpClient = new CtpClient();
     this.loginData = loginData;
+    this.LOCAL_STORAGE = new LocalStorage();
   }
 
   async signIn(): Promise<ClientResponse<CustomerSignInResult>> {
@@ -24,9 +30,27 @@ export class CustomerLogin {
         .me()
         .login()
         .post({
+          headers: {
+            Authorization: `${new LocalStorage().getLocalStorageItem(Constants.ACCESS_TOKEN_KEY) as string}`,
+          },
           body: customerLogin,
         })
         .execute();
+      if (response.statusCode === 200) {
+        const customerToken = await new Authorization().requestCustomerTokenInfo(
+          customerLogin.email,
+          customerLogin.password,
+        );
+        this.LOCAL_STORAGE.setLocalStorageItem(
+          Constants.ACCESS_TOKEN_KEY,
+          `${customerToken.token_type as string} ${customerToken.access_token}`,
+        );
+        this.LOCAL_STORAGE.setLocalStorageItem(Constants.REFRESH_TOKEN_KEY, customerToken.refresh_token);
+        this.LOCAL_STORAGE.setLocalStorageItem(
+          Constants.EXPIRES_IN_TOKEN_KEY,
+          `${new Date().getTime() + Number(customerToken.expires_in) * 1000}`,
+        );
+      }
       return response;
     } catch (error) {
       return Promise.reject(error);
